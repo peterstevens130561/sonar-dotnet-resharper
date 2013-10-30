60f0f32544e731e87405fa2cde9766b41acad72b
John M. Wright <john@wrightfully.com>
2013-10-29 23:42:13 -0500

    SONARPLUGINS-3202 updating rules based on new inspectcode /dumpIssuesTypes option
    based on EAP build of inspectcode 8.1.7.11

--- sonar-dotnet-resharper-plugin/src/test/java/com/wrightfully/sonar/plugins/dotnet/resharper/DefaultRulesTest.java
+++ sonar-dotnet-resharper-plugin/src/test/java/com/wrightfully/sonar/plugins/dotnet/resharper/DefaultRulesTest.java
@@ -19,41 +19,14 @@
  */
 package com.wrightfully.sonar.plugins.dotnet.resharper;
 
-import com.google.common.collect.Lists;
+import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.ReSharperFileParser;
+import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.ReSharperRule;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.sonar.api.batch.SensorContext;
-import org.sonar.api.resources.Project;
-import org.sonar.api.resources.ProjectFileSystem;
-import org.sonar.api.resources.Resource;
-import org.sonar.api.rules.Rule;
-import org.sonar.api.rules.RuleFinder;
-import org.sonar.api.rules.RuleQuery;
-import org.sonar.api.rules.Violation;
-import org.sonar.plugins.dotnet.api.DotNetResourceBridge;
-import org.sonar.plugins.dotnet.api.DotNetResourceBridges;
-import org.sonar.plugins.dotnet.api.microsoft.MicrosoftWindowsEnvironment;
-import org.sonar.plugins.dotnet.api.microsoft.VisualStudioProject;
-import org.sonar.plugins.dotnet.api.microsoft.VisualStudioSolution;
-import org.sonar.plugins.dotnet.api.utils.ResourceHelper;
-import org.sonar.test.TestUtils;
 
-import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.ReSharperRule;
-import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.ReSharperFileParser;
-
-import java.io.File;
-import java.nio.charset.Charset;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.*;
+import java.io.Reader;
+import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -66,6 +39,12 @@ public class DefaultRulesTest {
     public void init() {
         Reader fileReader = new InputStreamReader(getClass().getResourceAsStream("/com/wrightfully/sonar/plugins/dotnet/resharper/rules/DefaultRules.ReSharper"));
         _rules = ReSharperFileParser.parseRules(fileReader);
+    }
+
+
+    @Test
+    public void testDefaultRulesContainsRules() throws Exception {
+        assertThat(_rules.size()).isGreaterThan(1);
     }
 
     @Test
