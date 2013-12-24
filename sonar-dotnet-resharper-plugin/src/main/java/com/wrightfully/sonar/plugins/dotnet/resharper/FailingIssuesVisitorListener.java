5ef30acfd0944896a199f2812d00214696c122aa
Peter Stevens <peter.stevens@bakerhughes.com>
2013-12-24 13:22:41 +0000

    refactoring

---
+++ sonar-dotnet-resharper-plugin/src/main/java/com/wrightfully/sonar/plugins/dotnet/resharper/FailingIssuesVisitorListener.java
@@ -0,0 +1,94 @@
+/*
+ * Sonar .NET Plugin :: ReSharper
+ * Copyright (C) 2013 John M. Wright
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package com.wrightfully.sonar.plugins.dotnet.resharper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.Properties;
+import org.sonar.api.Property;
+import org.sonar.api.PropertyType;
+import org.sonar.api.utils.SonarException;
+
+import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.IssueModel;
+import com.wrightfully.sonar.plugins.dotnet.resharper.profiles.IssueListener;
+
+@Properties({
+    @Property(key = ReSharperConstants.FAIL_ON_ISSUES_KEY,
+    defaultValue = "CSharpError", name = "ReSharper issues to fail analysis on",
+    description = "Add IssueType values from ReSharper's results file for issues that should result in the analysis failing, i.e. CSharpErrors",
+            type = PropertyType.TEXT, global = true, project = false)
+})
+public class FailingIssuesVisitorListener implements IssueListener {
+    private static final Logger LOG = LoggerFactory.getLogger(FailingIssuesVisitorListener.class);
+    
+    private Collection<String> issuesToFailOn = new ArrayList<String>();
+    private List<IssueModel> issues =new ArrayList<IssueModel>();
+    
+
+	public void parsedIssue(IssueModel issue) {
+		if( issue == null) {
+			return ;
+		}
+		if(issuesToFailOn.contains(issue.getId())) {
+			issues.add(issue);
+		}
+	}
+	
+	/**
+	 * Provide comma seperated list of issueTypes on which the analysis should fail
+	 * @param issueTypes
+	 */
+	public void setIssueTypesToFailOn(String issueTypes) {
+		if(issueTypes == null) {
+			return;
+		}
+		for(String issue : issueTypes.split(",")) {
+			this.issuesToFailOn.add(issue);
+		}
+	}
+
+	
+	public Boolean hasMatches() {
+		return issues.size() > 0;
+	}
+	public int getErrorCount() {
+		return issues.size();
+	}
+
+	public void parsingComplete() {
+		if(hasMatches()) {
+			String msg = String.format("found %d issues that will cause the analysis to fail, please address first. Showing first %d issues\n",issues.size(),10);
+			LOG.error(msg);
+			throw new SonarException("Issues found that fail the analysis, please check the log");
+		}
+	}
+
+	public void start(ReSharperConfiguration configuration) {
+		String issueTypes=configuration.getString(ReSharperConstants.FAIL_ON_ISSUES_KEY);
+		LOG.debug(ReSharperConstants.FAIL_ON_ISSUES_KEY,issueTypes);
+        setIssueTypesToFailOn(issueTypes);
+	}
+
+}
